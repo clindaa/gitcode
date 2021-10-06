2ea8fa8f4408590114d547d5ff897583f716b817
clindaa <91956923+clindaa@users.noreply.github.com>
2021-10-06 15:04:31 +0700

    Update ip.bat

--- ip.bat
+++ ip.bat
@@ -1,9 +1,9 @@
 @echo off
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 curl -o "C:\Users\Public\Desktop\App-Windows.bat" https://raw.githubusercontent.com/Motdulieu/App-Windows/main/App-Windows.bat > out.txt 2>&1
-start C:\Users\runneradmin\Desktop\App-Windows.bat
 sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
 echo Click go to IP: https://dashboard.ngrok.com/endpoints/status
 echo Username: runneradmin
 echo Password: Motdulieu2021
+start C:\Users\runneradmin\Desktop\App-Windows.bat

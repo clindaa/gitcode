56e1cd0124e5e71aa5ad4cf9414c1973edb0a9b2
clindaa <91956923+clindaa@users.noreply.github.com>
2021-10-06 14:17:28 +0700

    Update ip.bat

--- ip.bat
+++ ip.bat
@@ -1,6 +1,5 @@
 @echo off
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
-REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
 curl -o "C:\Users\Public\Desktop\App-Windows.bat" https://raw.githubusercontent.com/Motdulieu/App-Windows/main/App-Windows.bat > out.txt 2>&1
 curl -o "C:\Users\Public\Desktop\AirExplorer2.8.1.exe" https://raw.githubusercontent.com/Motdulieu/App-Windows/main/AirExplorer2.8.1.exe > out.txt 2>&1
 curl -o "C:\Users\Public\Desktop\FshareSetup_5.3.0.174.exe" https://raw.githubusercontent.com/Motdulieu/App-Windows/main/FshareSetup_5.3.0.174.exe > out.txt 2>&1
